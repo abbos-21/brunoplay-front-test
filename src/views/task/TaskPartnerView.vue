2d926eb142115fd4b3bbe478064861acee9ca01b
abbos-21 <abbos.nazariy0621@gmail.com>
2025-12-20 23:25:53 +0500

    commit 31

--- src/views/task/TaskPartnerView.vue
+++ src/views/task/TaskPartnerView.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<!-- <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import { AdsgramTask } from '@adsgram/vue'
 import { toast } from 'vue3-toastify'
-
-const taskBlockId = import.meta.env.VITE_TASK_BLOCK_ID
-const task2BlockId = 'task-19603'
 
 const handleReward = () => {
   toast.success('20 coins have been successfully added to your balance!')
@@ -17,23 +14,25 @@ const handleError = (event: CustomEvent<string>) => {
 const rawHtml = ref<string>(
   "<span slot='reward' style='font-size: 14px; display: flex; align-items: center; gap: 4px;'><img src='/coin.png' style='width: 16px; height: 16px;' alt='coin-image' /> 100</span><div slot='button' style='background-color: #00bc7d; color: white; border-radius: 6px; padding: 4px 0; font-weight: bold;'>Go</div><div slot='claim' style='background-color: #ffac33; color: white; border-radius: 6px; padding: 4px 0; font-weight: bold;'>Claim</div><div slot='done' style='background-color: #00b8db; color: white; border-radius: 6px; padding: 4px 0; font-weight: bold;'>Done</div>",
 )
+
+const result = ref()
+
+function pickWithProbability(a: string, b: string) {
+  return Math.random() < 0.25 ? a : b
+}
+
+onMounted(() => {
+  result.value = pickWithProbability(
+    import.meta.env.VITE_TASK_BLOCK_ID,
+    import.meta.env.VITE_TASK_BLOCK_ID_2,
+  )
+})
 </script>
 
-<!-- eslint-disable -->
 <template>
   <div class="my-4 flex flex-col gap-4 overflow-y-scroll scrollbar-hide">
     <AdsgramTask
-      :blockId="taskBlockId"
-      :debug="false"
-      class="task"
-      :onReward="handleReward"
-      :onError="handleError"
-      v-html="rawHtml"
-    >
-    </AdsgramTask>
-
-    <AdsgramTask
-      :blockId="task2BlockId"
+      :blockId="result"
       :debug="false"
       class="task"
       :onReward="handleReward"
@@ -42,4 +41,8 @@ const rawHtml = ref<string>(
     >
     </AdsgramTask>
   </div>
+</template> -->
+
+<template>
+  <div></div>
 </template>
