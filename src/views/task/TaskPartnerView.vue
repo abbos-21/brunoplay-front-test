52f998bad1d63838f3d302932da88b97f4f95cea
abbos-21 <abbos.nazariy0621@gmail.com>
2025-12-20 19:39:24 +0500

    commit 25

--- src/views/task/TaskPartnerView.vue
+++ src/views/task/TaskPartnerView.vue
@@ -13,7 +13,7 @@ const handleError = (event: CustomEvent<string>) => {
 }
 
 const rawHtml = ref<string>(
-  "<span slot='reward' style='font-size: 14px; display: flex; align-items: center; gap: 4px;'><img src='/coin.png' style='width: 16px; height: 16px;' alt='coin-image' /> 100</span><div slot='button' style='background-color: #00bc7d; color: white; border-radius: 6px; padding: 4px; font-weight: bold; margin-left: 4px;'>Go</div><div slot='claim' style='background-color: #ffac33; color: white; border-radius: 6px; padding: 4px; font-weight: bold; margin-left: 4px;'>Claim</div><div slot='done' className=''>Done</div>",
+  "<span slot='reward' style='font-size: 14px; display: flex; align-items: center; gap: 4px;'><img src='/coin.png' style='width: 16px; height: 16px;' alt='coin-image' /> 100</span><div slot='button' style='background-color: #00bc7d; color: white; border-radius: 6px; padding: 4px; font-weight: bold;'>Go</div><div slot='claim' style='background-color: #ffac33; color: white; border-radius: 6px; padding: 4px; font-weight: bold;'>Claim</div><div slot='done' className=''>Done</div>",
 )
 </script>
 
